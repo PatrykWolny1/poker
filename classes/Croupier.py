2875caeff93ccc911915b710d203e3726eb82df1
patryk <patrykwo@gmail.com>
2024-06-23 09:20:03 +0200

    Deleted lines

--- classes/Croupier.py
+++ classes/Croupier.py
@@ -8,29 +8,14 @@ class Croupier(object):
     player2 = None
     def __init__(self):
         self.deck = Deck()
-<<<<<<< Updated upstream
         self.cards = [Card("4", "Ka"),
                       Card("2", "Pi"),
                       Card("K", "Tr"),
                       Card("K", "Ka"),
                       Card("K", "Ki")]
-<<<<<<< Updated upstream
+
         self.player1 = Player('Nick', self.deck, False)
         self.player2 = Player('Tom', self.deck, False)
-=======
-        self.cards = [Card("10", "Ka"),
-                      Card("2", "Pi"),
-                      Card("Q", "Tr"),
-                      Card("7", "Pi"),
-                      Card("J", "Ka")]
-        self.player1 = Player('Nick', self.deck, True)
-        #self.player2 = Player('Tom', self.deck, True)
->>>>>>> Stashed changes
-=======
-        self.player1 = Player('Nick', self.deck, True)
-        self.player2 = Player('Tom', self.deck, True)
->>>>>>> Stashed changes
-
 
     ###############################################ZROBIC KOMENTARZE DO HIGH_CARD I ONE_PAIR I THREE_OF_A_KIND
     def print(self):
@@ -42,11 +27,9 @@ class Croupier(object):
         self.player2.print()
 
         #self.player1.cards_permutations()
-<<<<<<< Updated upstream
         self.player1.print_arrangement()
-=======
+
         #self.player1.print_arrangement()
->>>>>>> Stashed changes
 
         #print()
         #self.player1.print()
